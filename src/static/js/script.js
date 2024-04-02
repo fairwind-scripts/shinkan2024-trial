0898be244914386f2a27cf28e4ea6724d4aa04a0
tomoszkfw <160325821+tomoszkfw@users.noreply.github.com>
2024-04-02 14:15:25 +0900

    feat: メニューを開閉するスクリプトを記述

---
+++ src/static/js/script.js
@@ -0,0 +1,48 @@
+const slideDown = (el) => {
+    el.style.height = "auto";
+    let h = el.offsetHeight;
+    el.style.height = h + "px";
+    el.animate([{ height: 0 }, { height: h + "px" }], {
+        duration: 250,
+    });
+};
+
+const slideUp = (el) => {
+    el.style.height = 0;
+};
+
+let activeIndex = null;
+
+const accordions = document.querySelectorAll("ul.include-accordion");
+accordions.forEach((accordion) => {
+    const accordionBtns = accordion.querySelectorAll(".accordionBtn");
+    accordionBtns.forEach((accordionBtn, index) => {
+        accordionBtn.addEventListener("click", (e) => {
+            activeIndex = index;
+            e.currentTarget.parentNode.classList.toggle("active");
+            const content = accordionBtn.nextElementSibling;
+            if (e.currentTarget.parentNode.classList.contains("active")) {
+                slideDown(content);
+            } else {
+                slideUp(content);
+            }
+            accordionBtns.forEach((btn, i) => {
+                if (activeIndex !== i) {
+                    btn.parentNode.classList.remove("active");
+                    const openedContent = btn.nextElementSibling;
+                    slideUp(openedContent);
+                }
+            });
+            let container = accordion.closest(".scroll-control");
+            if (
+                e.currentTarget.parentNode.classList.contains("active") ==
+                    false &&
+                container !== null
+            ) {
+                container.classList.remove("active");
+            } else if (container !== null) {
+                container.classList.add("active");
+            }
+        });
+    });
+});
